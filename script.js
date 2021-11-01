7fd0e7c68d7d70b8efb3bf7f9a5897029c5f1a58
Vamshi-Krishna-Jillela <89860169+Vamshi-Krishna-Jillela@users.noreply.github.com>
2021-11-01 19:32:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+console.log("see the code for more clarification:\n");
+var c=12;
+console.log(" variable c is declared and also intialised\n")
+console.log("the value of c: "+c);
+var a;
+console.log(" variable a is declared, but not intialised\n")
+console.log("the value of a: "+a);
+console.log(" variable b is not introduced\n")
+console.log("the value of b: "+b);
+
